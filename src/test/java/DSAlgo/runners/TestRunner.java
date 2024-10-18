71432fb45dd5c3ca6ef50939ab04adc96144a82c
Sumalatha <sumateegala06@gmail.com>
2024-10-18 10:57:17 -0400

    Added allure and extent reports dependencies.

--- src/test/java/DSAlgo/runners/TestRunner.java
+++ src/test/java/DSAlgo/runners/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", // Path to feature files
 		glue = { "stepDefinitions" }, // Path to step definitions package
-		plugin = { "pretty", "html:target/cucumber-reports.html" }, // For reporting
+		plugin = { "pretty", "html:target/cucumber-reports.html" ,"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, // For reporting
 		monochrome = true // For better console output
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
