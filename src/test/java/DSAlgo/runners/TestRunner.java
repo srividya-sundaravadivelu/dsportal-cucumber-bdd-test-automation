4079126914ca386fe0f932a8b6ccf57a4803d158
srividya-sundaravadivelu <srividya-sundaravadivelu>
2024-10-24 11:23:08 -0500

    included extent report

--- src/test/java/DSAlgo/runners/TestRunner.java
+++ src/test/java/DSAlgo/runners/TestRunner.java
@@ -9,7 +9,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", // Path to feature files
 		glue = { "stepDefinitions","hooks" }, // Path to step definitions package
-		plugin = { "pretty", "html:target/cucumber-reports.html" ,"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, // For reporting
+		plugin = { "pretty", "html:target/cucumber-reports.html" ,
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, // For reporting
 		monochrome = true // For better console output
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
