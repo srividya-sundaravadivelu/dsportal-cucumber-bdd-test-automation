e94c9b3f84e5b9511e635a5093d40c2806fb75fc
Sumalatha <sumateegala06@gmail.com>
2024-10-29 22:08:54 -0400

    Added feature files and Step Definitions for Array and Linked List modules

---
+++ src/test/java/stepDefinitions/ArraySteps.java
@@ -0,0 +1,298 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ArraySteps {
+	
+	@Given("The user is logged in with username {string} and password {string}")
+	public void the_user_is_logged_in_with_username_and_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Home page")
+	public void the_user_is_on_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Array item from the drop down menu")
+	public void the_user_clicks_the_array_item_from_the_drop_down_menu() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Array page")
+	public void the_user_should_be_redirected_to_array_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Get Started button in Array Panel")
+	public void the_user_clicks_the_get_started_button_in_array_panel() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Array page")
+	public void the_user_should_be_redirected_to_the_array_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Arrays in Python link")
+	public void the_user_clicks_arrays_in_python_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Overview of Arrays in Python page")
+	public void the_user_should_be_redirected_to_the_overview_of_arrays_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Arrays in Python page")
+	public void the_user_is_on_the_arrays_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button on {string} Page")
+	public void the_user_clicks_button_on_page(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Try Editor page")
+	public void the_user_should_be_redirected_to_the_try_editor_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks of Arrays Using List link")
+	public void the_user_clicks_of_arrays_using_list_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Overview of Arrays Using List page")
+	public void the_user_should_be_redirected_to_the_overview_of_arrays_using_list_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Arrays Using List page")
+	public void the_user_is_on_the_arrays_using_list_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks  of Basic Operations in Lists link")
+	public void the_user_clicks_of_basic_operations_in_lists_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Basic Operations in Lists page")
+	public void the_user_should_be_redirected_to_the_basic_operations_in_lists_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Basic Operations in Lists page")
+	public void the_user_is_on_the_basic_operations_in_lists_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks  Applications of Array link")
+	public void the_user_clicks_applications_of_array_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Applications of Array page")
+	public void the_user_should_be_redirected_to_the_applications_of_array_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Applications of Array page")
+	public void the_user_is_on_the_applications_of_array_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Practice Questions link")
+	public void the_user_clicks_practice_questions_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the Practice Questions page")
+	public void the_user_should_be_redirected_to_the_practice_questions_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on Practice Questions page")
+	public void the_user_is_on_practice_questions_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks search the array link")
+	public void the_user_clicks_search_the_array_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to {string} page contains a question,an tryEditor with Run and Submit buttons")
+	public void the_user_should_be_redirected_to_page_contains_a_question_an_try_editor_with_run_and_submit_buttons(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Max Consecutive Ones link")
+	public void the_user_clicks_max_consecutive_ones_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Find Numbers with Even Number of Digits link")
+	public void the_user_clicks_find_numbers_with_even_number_of_digits_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Squares of a Sorted Array link")
+	public void the_user_clicks_squares_of_a_sorted_array_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("The user should be redirected to QUESTION page contains a question,an tryEditor with Run and Submit buttons")
+	public void the_user_should_be_redirected_to_question_page_contains_a_question_an_try_editor_with_run_and_submit_buttons() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Python Editor of Practice Page")
+	public void the_user_is_on_the_python_editor_of_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("The user clicks the Run Button without entering the code in the Editor of Practice Page")
+	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor_of_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Nothing happens to the page and no error message is displayed in Practice Page")
+	public void nothing_happens_to_the_page_and_no_error_message_is_displayed_in_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor of Practice Page Click the Run Button")
+	public void the_user_write_the_invalid_code_in_editor_of_practice_page_click_the_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see an error message in alert window of Practice Page")
+	public void the_user_should_able_to_see_an_error_message_in_alert_window_of_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor of Practice Page Click the Run Button")
+	public void the_user_write_the_valid_code_in_editor_of_practice_page_click_the_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see output in the console of PracticePage")
+	public void the_user_should_able_to_see_output_in_the_console_of_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor of Practice Page Click the Submit Button")
+	public void the_user_write_the_valid_code_in_editor_of_practice_page_click_the_submit_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user see an error message Error occurred during submission")
+	public void the_user_see_an_error_message_error_occurred_during_submission() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("The user is on the Try Editor page")
+	public void the_user_is_on_the_try_editor_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters valid Python code from sheet {string} and row {int} and clicks the Run button")
+	public void the_user_enters_valid_python_code_from_sheet_and_row_and_clicks_the_run_button(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user is able to see the output inside the console")
+	public void the_user_is_able_to_see_the_output_inside_the_console() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters invalid Python code from sheet {string} and row {int} and clicks the Run button")
+	public void the_user_enters_invalid_python_code_from_sheet_and_row_and_clicks_the_run_button(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}	
+
+	@Then("The user sees an error message in the alert window")
+	public void the_user_sees_an_error_message_in_the_alert_window() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Run Button without entering code in the Editor")
+	public void the_user_clicks_the_run_button_without_entering_code_in_the_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The focus remains in the same page and no error message is displayed")
+	public void the_focus_remains_in_the_same_page_and_no_error_message_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Try Editor page with an alert error message")
+	public void the_user_is_on_the_try_editor_page_with_an_alert_error_message() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the OK button in the alert window")
+	public void the_user_clicks_the_ok_button_in_the_alert_window() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user is on the same page having the Editor and Run button")
+	public void the_user_is_on_the_same_page_having_the_editor_and_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
---
+++ src/test/resources/features/linkedlist.feature
@@ -0,0 +1,122 @@
+Feature:  Testing Linked List feature in dsportalapp.herokuapp
+ User should be able to be in Home page
+ 
+  Background: 
+    Given The user is logged in with username "numpy@gmail.com" and password "Tester123$"
+    
+    @tag1
+  Scenario: Redirect to Linked List page from Home page drop down menu 
+    Given The user is on the Home page
+    When The user clicks the Linked List item from the drop down menu
+    Then The user should be redirected to Linked List page
+   
+   Scenario: Redirect to Linked List page from Get Started button in Linked List Panel 
+		Given The user is on the Home page	
+		When The user clicks the Get Started button in Linked List Panel
+		Then The user should be redirected to the Linked List page 
+		
+		#Introduction
+		Scenario: Redirect to Introduction Page from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Introduction link
+		Then The user should be redirected to Introduction page
+		
+		Scenario: Redirect to Editor from Introduction Page
+		Given The user is on the Introduction page
+		When The user clicks "Try Here" button on "Introduction" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Creating Linked List Link
+		Scenario: Redirect to Creating Linked List Page from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Creating Linked LIst link
+		Then The user should be redirected to "Creating Linked List" page
+		
+		Scenario: Redirect to Editor from Creating Linked List page
+		Given The user is on the Creating Linked List page
+		When The user clicks "Try Here" button on "Creating Linked List" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Types of Linked List		
+		Scenario: Redirect to Types of Linked List from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Types of Linked List link
+		Then The user should be redirected to Types of Linked List page
+		
+		Scenario: Redirect to Editor from Types of Linked List Page
+		Given The user is on the Types of Linked List page
+		When The user clicks "Try Here" button on "Types of Linked List" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Implement Linked List in Python
+		Scenario: Redirect to Implement Linked List in Page from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Implement Linked List in Python link
+		Then The user should be redirected to Implement Linked List in Python page
+		
+		Scenario:
+		Given The user is on the Implement Linked List in Python page
+		When The user clicks "Try Here" button on "Implement Linked List in Python" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Traversal
+		Scenario: Redirect to Traversal Page from Linked List Page 
+		Given The user is on the Linked List page
+		When The user clicks Traversal link
+		Then The user should be redirected to Traversal page
+		
+		Scenario: Redirect to Editor from Traversal Page
+		Given The user is on the Traversal page
+		When The user clicks "Try Here" button on "Traversal" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Insertion
+		Scenario: Redirect to Insertion Page from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Insertion link
+		Then The user should be redirected to Insertion page
+		
+		Scenario: Redirect to Editor from Insertion Page
+		Given The user is on the Insertion page
+		When The user clicks "Try Here" button on "Insertion" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		#Deletion
+		Scenario: Redirected to Deletion Page from Linked List Page
+		Given The user is on the Linked List page
+		When The user clicks Deletion link
+		Then The user should be redirected to Deletion page.
+		
+		Scenario: Redirected to Editor from Deletion page
+		Given The user is on the Deletion page
+		When The user clicks "Try Here" button on "Deletion" Page
+		Then The user should be redirected to a page having Editor and run button
+		
+		# Try Editor Page
+  Scenario Outline:: Run Button with valid code in Python Code Editor Page
+    Given The user is on the Try Editor page	
+    When The user enters valid Python code from sheet "<SheetName>" and row <RowNumber> and clicks the Run button	
+    Then The user is able to see the output inside the console
+     Examples:
+	    | SheetName  | RowNumber |
+	    | PythonCode | 0         |
+    
+  Scenario Outline: Run Button with invalid code in Python Code Editor Page
+    Given The user is on the Try Editor page	
+    When  The user enters invalid Python code from sheet "<Sheetname>" and row <RowNumber> and clicks the Run button
+    Then The user sees an error message in the alert window
+      Examples:
+    | SheetName  | RowNumber |
+    | PythonCode | 1         |
+    
+  Scenario: Run Button without entering code in Python Code Editor Page
+    Given The user is on the Try Editor page	
+    When The user clicks the Run Button without entering code in the Editor	
+    Then The focus remains in the same page and no error message is displayed
+    
+  Scenario: Handle alert message in Python Code Editor Page   
+    Given The user is on the Try Editor page with an alert error message	
+    When The user clicks the OK button in the alert window	
+    Then The user is on the same page having the Editor and Run button
+		
+		
